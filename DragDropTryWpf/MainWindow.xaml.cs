fb60416e3e537b0e60c69ef9c68eeedc62d3f018
Suman Karan <xpd532@gmail.com>
2018-06-25 11:55:33 +0530

    Add project files.

---
+++ DragDropTryWpf/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using DragDropTryWpf.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF.JoshSmith.ServiceProviders.UI;
+
+namespace DragDropTryWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ListViewDragDropManager<DragableItem> listViewDragDropManager;
+        ListViewDragDropManager<DragableItem> listViewDragDropManager2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new MainViewModel();
+            // this.Loaded += mainWindow_Loaded;
+            listViewDragDropManager = new ListViewDragDropManager<DragableItem>(this.dragableListView) { IsDeleteSourceItem = true };
+            listViewDragDropManager2 = new ListViewDragDropManager<DragableItem>(this.dragableListView2) { IsDeleteSourceItem = true };
+        }
+
+        private void mainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ DragDropTryWpf/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Mvvm;
+using Mvvm.Commands;
+using System.Collections.ObjectModel;
+
+namespace DragDropTryWpf.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        public ObservableCollection<DragableItem> DragableCollection { get; set; } 
+        public ObservableCollection<DragableItem> DragableCollection2 { get; set; }
+
+        public DelegateCommand ClickToAddNewItemCommand { get; set; }
+
+        public MainViewModel()
+        {
+
+            var dataCollection = new DragableItem[5];
+            for (int i = 0; i < dataCollection.Length; i++)
+            {
+                dataCollection[i] = new DragableItem { Name = $"Item{i}" };
+            }
+            DragableCollection = new ObservableCollection<DragableItem>(dataCollection);
+
+            dataCollection = new DragableItem[5];
+            for (int i = 0; i < dataCollection.Length; i++)
+            {
+                dataCollection[i] = new DragableItem { Name = $"Item2{i}" };
+            }
+            DragableCollection2 = new ObservableCollection<DragableItem>(dataCollection);
+
+
+            ClickToAddNewItemCommand = new DelegateCommand(() => DragableCollection.Insert(3, new DragableItem { Name = "New Item" }));
+        }
+    }
+
+    public class DragableItem
+    {
+        public string Name { get; set; }
+    }
+}
